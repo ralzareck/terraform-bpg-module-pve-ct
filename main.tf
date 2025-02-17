5c23a4bce192f8a54f7748bc7c4a9bb73a4046b7
RalZareck <axelgrunig@orange.fr>
2025-02-17 01:11:54 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,274 @@
+# =============================================================================
+# ===== Provider ==============================================================
+# =============================================================================
+
+terraform {
+  required_providers {
+    proxmox = {
+      version = ">= 0.66"
+      source  = "bpg/proxmox"
+    }
+    random = {
+      version = "~> 3"
+      source  = "hashicorp/random"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0"
+    }
+  }
+}
+
+
+# =============================================================================
+# = CT Creation ===============================================================
+# =============================================================================
+
+locals {
+  test_user_passwd = var.ct_init.user == null ? null : var.ct_init.user.password
+  test_user_keys = var.ct_init.user == null ? null : var.ct_init.user.keys
+}
+
+
+resource "random_password" "ct_root_pw" {
+  count            = (local.test_user_passwd == null && var.ct_type == "template") ? 1 : 0
+  override_special = "_%@"
+  special          = true
+  length           = 30
+}
+
+resource "proxmox_virtual_environment_container" "pve_ct" {
+  # Proxmox
+  node_name    = var.pve_node
+
+  # CT Information
+  description  = var.ct_description
+  tags         = var.ct_tags
+  vm_id        = var.ct_id
+  pool_id      = var.ct_pool
+  unprivileged = var.ct_unprivileged
+  protection   = var.ct_protection
+  template     = var.ct_template
+
+  # Boot settings
+  started      = var.ct_start.on_deploy
+  start_on_boot = var.ct_start.on_boot
+
+  startup {
+    order      = var.ct_start.order
+    up_delay   = var.ct_start.up_delay
+    down_delay = var.ct_start.down_delay
+  }
+
+  dynamic "clone" {
+    for_each = (var.ct_type == "clone") ? ["enabled"] : []
+    content {
+      datastore_id = var.src_clone.datastore_id
+      node_name    = (var.src_clone.node_name != null) ? var.src_clone.node_name : var.pve_node
+      vm_id        = var.src_clone.tpl_id
+    }
+  }
+
+  # VM Configuration
+  dynamic "operating_system" {
+    for_each = (var.ct_type == "template") ? ["enabled"] : []
+    content {
+      # template_file_id  = (var.src_file.url == null) ? "${var.src_file.datastore_id}:vztmpl/${var.src_file.file_name}" : proxmox_virtual_environment_download_file.ct_template[0].id
+      template_file_id  = "${var.src_file.datastore_id}:vztmpl/${var.src_file.file_name}"
+      type              = var.ct_os
+    }
+  }
+
+  cpu {
+    architecture = var.ct_cpu.arch
+    cores        = var.ct_cpu.cores
+    units        = var.ct_cpu.units
+  }
+
+  memory {
+    dedicated = var.ct_mem.dedicated
+    swap      = var.ct_mem.swap
+  }
+
+  disk {
+    datastore_id = var.ct_disk.datastore_id
+    size         = var.ct_disk.size
+  }
+
+  console {
+    enabled   = var.ct_console.enabled
+    type      = var.ct_console.type
+    tty_count = var.ct_console.tty_count
+  }
+
+  features {
+    nesting = var.ct_features.nesting
+    fuse    = var.ct_features.fuse
+    keyctl  = var.ct_features.keyctl
+    mount   = var.ct_features.mount
+  }
+
+  dynamic "network_interface" {
+    for_each = var.ct_net_ifaces
+    content {
+      name         = network_interface.value.name
+      bridge       = network_interface.value.bridge
+      enabled      = network_interface.value.enabled
+      firewall     = network_interface.value.firewall
+      mac_address  = network_interface.value.mac_addr
+      mtu          = network_interface.value.mtu
+      rate_limit   = network_interface.value.rate_limit
+      vlan_id      = network_interface.value.vlan_id
+    }
+  }
+
+  initialization {
+    hostname = var.ct_name
+
+    dynamic "ip_config" {
+      for_each = var.ct_net_ifaces
+      content {
+        ipv4 {
+          address = ip_config.value.ipv4_addr
+          gateway = ip_config.value.ipv4_gw
+        }
+      }
+    }
+
+    dynamic "dns" {
+      for_each = (var.ct_init.dns != null) ? ["enabled"] : []
+      content {
+        domain  = var.ct_init.dns.domain
+        servers = var.ct_init.dns.servers
+      }
+    }
+
+    dynamic "user_account" {
+      for_each = (var.ct_type == "template") ? ["enabled"] : []
+      content {
+        password = local.test_user_passwd != null ? var.ct_init.user.password : random_password.ct_root_pw[0].result
+        keys     = local.test_user_keys != null ? var.ct_init.user.keys : []
+      }
+    }
+  }
+}
+
+# =============================================================================
+# = CT Firewall ===============================================================
+# =============================================================================
+
+resource "proxmox_virtual_environment_firewall_options" "pve_ct_fw_opts" {
+  count = (var.ct_fw_opts != null) ? 1 : 0
+
+  node_name     = proxmox_virtual_environment_container.pve_ct.node_name
+  vm_id         = proxmox_virtual_environment_container.pve_ct.vm_id
+
+  enabled       = var.ct_fw_opts.enabled
+  dhcp          = var.ct_fw_opts.dhcp
+  input_policy  = var.ct_fw_opts.input_policy
+  output_policy = var.ct_fw_opts.output_policy
+  macfilter     = var.ct_fw_opts.macfilter
+  ipfilter      = var.ct_fw_opts.ipfilter
+}
+
+resource "proxmox_virtual_environment_firewall_rules" "pve_ct_fw_rules" {
+  count = (var.ct_fw_rules != null || var.ct_fw_group != null) ? 1 : 0
+
+  node_name    = proxmox_virtual_environment_container.pve_ct.node_name
+  vm_id        = proxmox_virtual_environment_container.pve_ct.vm_id
+
+  dynamic "rule" {
+    for_each = var.ct_fw_rules != null ? var.ct_fw_rules : {}
+    content {
+      enabled = rule.value.enabled
+      action  = rule.value.action
+      type    = rule.value.direction
+      iface   = rule.value.iface
+      proto   = rule.value.proto
+      source  = rule.value.srcip
+      sport   = rule.value.srcport
+      dest    = rule.value.destip
+      dport   = rule.value.destport
+      comment = "${rule.value.comment == null ? "" : rule.value.comment}; Managed by Terraform"
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.ct_fw_group != null ? var.ct_fw_group : {}
+    content {
+      enabled        = rule.value.enabled
+      security_group = rule.key
+      iface          = rule.value.iface
+      comment        = "${rule.value.comment == null ? "" : rule.value.comment}; Managed by Terraform"
+    }
+  }
+}
+
+# =============================================================================
+# = CT Bootstrap ==============================================================
+# =============================================================================
+
+resource "time_sleep" "wait_for_ct" {
+  count           = length(var.ct_bootstrap) > 0 ? 1 : 0
+  create_duration = "10s"
+  triggers = {
+    id = proxmox_virtual_environment_container.pve_ct.id
+  }
+  depends_on = [
+    proxmox_virtual_environment_container.pve_ct
+  ]
+}
+
+resource "terraform_data" "bootstrap_ct" {
+  count = length(var.ct_bootstrap)
+
+  connection {
+    type        = "ssh"
+    host        = replace(proxmox_virtual_environment_container.pve_ct.initialization[0].ip_config[0].ipv4[0].address, "/24", "")
+    user        = "root"
+    private_key = file(var.ct_ssh_privkey)
+  }
+  provisioner "file" {
+    source      = values(var.ct_bootstrap)[count.index].script_path
+    destination = "/tmp/bootstrap_script${count.index}.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap_script${count.index}.sh",
+      join(" ", compact(["/tmp/bootstrap_script${count.index}.sh", try(values(var.ct_bootstrap)[count.index].arguments, [""])])),
+    ]
+  }
+
+  triggers_replace = [
+    time_sleep.wait_for_ct[0].id,
+    proxmox_virtual_environment_container.pve_ct.id
+  ]
+
+  depends_on = [
+    time_sleep.wait_for_ct
+  ]
+
+  lifecycle {
+    precondition {
+      condition     = try(values(var.ct_bootstrap)[count.index].script_path, null) != null
+      error_message = "Bootstrap script cannot be executed without script path."
+    }
+    precondition {
+      condition     = length(var.ct_net_ifaces) > 0
+      error_message = "Bootstrap script cannot be executed without a network interface."
+    }
+    precondition {
+      condition     = try(values(var.ct_net_ifaces)[0].ipv4_addr, null) != null
+      error_message = "Bootstrap script cannot be executed without ipv4_addr."
+    }
+    precondition {
+      condition     = var.ct_init.user.keys != null
+      error_message = "Bootstrap script cannot be executed without public ssh key."
+    }
+    precondition {
+      condition     = var.ct_ssh_privkey != null
+      error_message = "Bootstrap script cannot be executed without ssh private key."
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,286 @@
+# =============================================================================
+# ===== General ===============================================================
+# =============================================================================
+
+variable "pve_node" {
+  type              = string
+  description       = "PVE Node name on which the VM will be created on."
+
+  validation {
+    condition     = can(regex("[A-Za-z0-9.-]{1,63}", var.pve_node))
+    error_message = "This variable is constrained by the node name requirements set forth by ProxmoxVE."
+  }
+}
+
+variable "ct_type" {
+  type              = string
+  description       = "The source type used for the creation of the container. Can either be 'clone' or 'template'."
+
+  validation {
+    condition     = contains(["clone", "template"], var.ct_type)
+    error_message = "Valid values for var: ct_type are (clone, template)."
+  }
+}
+
+variable "ct_template" {
+  type        = bool
+  description = "Defines if the container should be converted to a template or not."
+  default     = false
+}
+
+variable "src_clone" {
+  type = object({
+    datastore_id = string
+    node_name    = optional(string)
+    tpl_id       = number
+  })
+  description    = "The target Container to clone. Can not be used with 'src_file'"
+  nullable       = true
+  default        = null
+}
+
+variable "src_file" {
+  type = object({
+    datastore_id        = string
+    file_name           = string
+  })
+  description           = "The target ISO file to use as base for the Container. Cannot be used with 'src_clone'"
+  nullable              = true
+  default               = null
+}
+
+# =============================================================================
+# ===== Container =============================================================
+# =============================================================================
+
+variable "ct_name" {
+  type        = string
+  description = "The name of the Container."
+}
+
+variable "ct_id" {
+  type        = number
+  description = "The ID of the Container."
+}
+
+variable "ct_description" {
+  type        = string
+  description = "The description of the Container."
+  nullable    = true
+  default     = null
+}
+
+variable "ct_unprivileged" {
+  type        = bool
+  description = "Defines if the container runs as unprivileged on the host."
+  default     = true
+}
+
+variable "ct_protection" {
+  type        = bool
+  description = "Defines if protection is enabled on the container."
+  default     = false
+}
+
+variable "ct_pool" {
+  type        = string
+  description = "The Pool in which to place the Container."
+  nullable    = true
+  default     = null
+
+  validation {
+    condition     = can(regex("[A-Za-z0-9_-]{0,63}", var.ct_pool)) || var.ct_pool == null
+    error_message = "value"
+  }
+}
+
+variable "ct_tags" {
+  type = list(string)
+  description = "A list of tags associated to the Container."
+  default     = []
+}
+
+variable "ct_start" {
+  type = object({
+    on_deploy  = bool
+    on_boot    = bool
+    order      = optional(number, 0)
+    up_delay   = optional(number, 0)
+    down_delay = optional(number, 0)
+  })
+  description  = "Defines startup and shutdown behavior of the container. "
+  default      = {
+    on_deploy  = true
+    on_boot    = false
+    order      = 0
+    up_delay   = 0
+    down_delay = 0
+  }
+}
+
+variable "ct_os" {
+  type = string
+  description = "The Operating System configuration of the container."
+  default = "unmanaged"
+
+  validation {
+    condition     = contains(["alpine", "archlinux", "centos", "debian", "devuan", "fedora", "gentoo", "nixos", "opensuse", "ubuntu", "unmanaged"], var.ct_os)
+    error_message = "Valid values for var: ct_os are (alpine, archlinux, centos, debian, devuan, fedora, gentoo, nixos, opensuse, ubuntu, unmanaged)."
+  }
+}
+
+variable "ct_cpu" {
+  type = object({
+    arch      = optional(string)
+    cores     = optional(number, 2)
+    units     = optional(number)
+  })
+  description = "The CPU Configuration of the container."
+  default     = {}
+}
+
+variable "ct_mem" {
+  type = object({
+    dedicated = optional(number, 2048)
+    swap      = optional(number)
+  })
+  description = "The Memory Configuration of the container."
+  default     = {}
+}
+
+variable "ct_console" {
+  type = object({
+    enabled   = optional(bool, true)
+    type      = optional(string, "shell")
+    tty_count = optional(number, 2)
+  })
+  description = "The Console Configuration of the container."
+  default     = {}
+}
+
+variable "ct_features" {
+  type = object({
+    nesting = optional(bool, false)
+    fuse    = optional(bool)
+    keyctl  = optional(bool)
+    mount   = optional(list(string))
+  })
+  description = "The container feature flags. Changing flags (except nesting) is only allowed for root@pam authenticated user."
+  default     = {}
+}
+
+variable "ct_disk" {
+  type = object({
+    datastore_id = string
+    size         = number
+  })
+  description    = "The Disks configuration of the container."
+}
+
+variable "ct_net_ifaces" {
+  type = map(object({
+    name       = string
+    bridge     = string
+    enabled    = optional(bool, true)
+    firewall   = optional(bool, true)
+    mac_addr   = optional(string)
+    model      = optional(string, "virtio")
+    mtu        = optional(number, 1500)
+    rate_limit = optional(string)
+    vlan_id    = optional(number)
+    ipv4_addr  = string
+    ipv4_gw    = string
+  }))
+  description  = "The network interfaces configuration of the container."
+  default      = {}
+
+  validation {
+    condition     = alltrue([for k,v in var.ct_net_ifaces : can(regex("net\\d+", k))])
+    error_message = "The IDs (keys) of the network interfaces must respect the following convention: net[number]."
+  }
+}
+
+variable "ct_init" {
+  type = object({
+    user       = optional(object({
+      password = optional(string)
+      keys     = optional(list(string))
+    }))
+    dns        = optional(object({
+      domain   = optional(string)
+      servers  = optional(list(string))
+    }))
+  })
+  description  = "The initialization configuration of the container."
+  default      = {}
+}
+
+# =============================================================================
+# ===== Host Firewall =========================================================
+# =============================================================================
+
+variable "ct_fw_opts" {
+  type = object({
+    enabled       = bool
+    dhcp          = optional(bool)
+    input_policy  = optional(string)
+    output_policy = optional(string)
+    macfilter     = optional(bool)
+    ipfilter      = optional(bool)
+    ndp           = optional(bool)
+    radv          = optional(bool)
+  })
+  description     = "Firewall settings of the container."
+  nullable        = true
+  default         = null
+}
+
+variable "ct_fw_rules" {
+  type = map(object({
+    enabled   = optional(bool, true)
+    action    = string
+    direction = string
+    iface     = optional(string)
+    proto     = optional(string)
+    srcip     = optional(string)
+    srcport   = optional(string)
+    destip    = optional(string)
+    destport  = optional(string)
+    comment   = optional(string)
+  }))
+  description = "Firewall rules of the container."
+  nullable    = true
+  default     = null
+}
+
+
+variable "ct_fw_group" {
+  type = map(object({
+    enabled   = optional(bool, true)
+    iface     = optional(string)
+    comment   = optional(string)
+  }))
+  description = "Firewall Security Groups of the container."
+  nullable    = true
+  default     = null
+}
+
+# =============================================================================
+# ===== Bootstrap =============================================================
+# =============================================================================
+
+variable "ct_ssh_privkey" {
+  type        = string
+  description = "File containing ssh private key to be used for container bootstrap."
+  nullable    = true
+  default     = null
+}
+
+variable "ct_bootstrap" {
+  type = map(object({
+    script_path = optional(string)
+    arguments   = optional(string)
+  }))
+  description = "List of paths to script files to be executed after container creation. Scripts will be executed in the order provided."
+  default     = {}
+}
